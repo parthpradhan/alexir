592eae836b012c0c5eca6ce855188f27335eae96
Parth Pradhan <parthdbz24@gmail.com>
2022-02-24 18:16:54 +0530

    Updates

---
+++ precache-manifest.2140a4f1d65181f7127d703f89c93165.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7a00e646eb7b2554342f76ca476f15c",
+    "url": "/alexir/index.html"
+  },
+  {
+    "revision": "7f2b5f882c6d7113a4b7",
+    "url": "/alexir/static/css/main.92af58e0.chunk.css"
+  },
+  {
+    "revision": "992ec9291d97e89283f1",
+    "url": "/alexir/static/js/2.040954af.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/alexir/static/js/2.040954af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f2b5f882c6d7113a4b7",
+    "url": "/alexir/static/js/main.4fb0bda9.chunk.js"
+  },
+  {
+    "revision": "7f9b6076689eb4c10041",
+    "url": "/alexir/static/js/runtime-main.943c65e6.js"
+  },
+  {
+    "revision": "a986e209aa3f4ec0d8eb03feb7522e2d",
+    "url": "/alexir/static/media/logo.a986e209.png"
+  }
+]);
